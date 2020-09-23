5b548a6977062b5b0b4bc537f9bbca42b1f1a449
marco79423 <marco79423@gmail.com>
2020-09-23 20:26:01 +0800

    修復 setup.py 不能下載 requirements.txt 的問題

--- setup.py
+++ setup.py
@@ -5,8 +5,12 @@ import setuptools
 
 base_dir = os.path.abspath(os.path.dirname(__file__))
 
+REQUIREMENTS = []
 with open(os.path.join(base_dir, 'requirements.txt'), encoding='utf-8') as fp:
-    REQUIREMENTS = fp.read().split()
+    for line in fp.readlines():
+        line = line.strip()
+        if line and not line.startswith('#'):
+            REQUIREMENTS.append(line)
 
 with open(os.path.join(base_dir, 'README.md'), encoding='utf-8') as fp:
     long_description = fp.read()
